9795576c5f731e992a804c690886629d9502f05a
Cristhian Custodio <cryisthian_06@hotmail.com>
2024-08-25 19:26:34 -0500

    creacion de api

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paciente;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Paciente::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = Paciente::create($request->all());
+        return response()->json($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {       
+        $data = Paciente::findOrFail($id);
+        return response()->json($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $paciente = Paciente::findOrFail($id);
+        $paciente->update($request->all());
+        return response()->json($paciente);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $paciente = Paciente::findOrFail($id);
+        $paciente->delete();
+        return response()->json($paciente);
+    }
+}
